300a974627799b553c812ed85a384d2d26306012
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2021-10-10 18:28:15 +0200

    Stage 2/3 - to upper and lower case ok

--- Case Converter/task/src/scripts/script.js
+++ Case Converter/task/src/scripts/script.js
@@ -1,13 +1,9 @@
-/**/
-function convertToUpperCase() {
+/* type: string */
+function convert(targetType) {
     let textArea = document.getElementById("text");
 
-    let convertedText = convertText(textArea.value);
+    let convertedText = convertText(textArea.value, targetType);
     textArea.value = convertedText;
-}
-
-function convertToLowerCase() {
-
 }
 
 function convertToProperCase() {
@@ -19,8 +15,29 @@ function convertToSentenceCase() {
 }
 
 /**/
-function convertText(text, method) {
-    console.log(`Text to convert: ${text}`);
-    let convertedText = text + " converted";
+function convertText(text, targetType) {
+    // console.log(`Text to convert: ${text}`);
+    let convertedText;
+    const myName = "convertText(): ";
+
+    switch (targetType) {
+        case "upper case":
+            convertedText = text.toUpperCase()
+            console.log(`${myName} process text to ${targetType}.`);
+            break;
+        case "lower case":
+            convertedText = text.toLowerCase()
+            console.log(`${myName} process text to ${targetType}.`);
+            break;
+        case "proper case":
+
+            break;
+        case "sequence case":
+
+            break;
+        default:
+            console.log(`Unknown target type ${targetType}. Can't process conversion.`);
+    }
+
     return convertedText;
 }
