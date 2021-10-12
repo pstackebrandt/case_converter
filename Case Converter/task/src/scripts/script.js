a5cbe882850962b4adf06b6a62c980db357e0bb9
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2021-10-12 10:23:42 +0200

    Stage 2/3 green - Proper case ok - Sentence case, simple solution for test text only - Design improved - Design specified for smartphone und ipad pro (both portrait mode)

--- Case Converter/task/src/scripts/script.js
+++ Case Converter/task/src/scripts/script.js
@@ -6,8 +6,10 @@ function convert(targetType) {
     textArea.value = convertedText;
 }
 
-function convertToProperCase() {
-
+function addDummyText() {
+    let textArea = document.getElementById("text");
+    textArea.value = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+    console.log(addDummyText);
 }
 
 function convertToSentenceCase() {
@@ -30,10 +32,16 @@ function convertText(text, targetType) {
             console.log(`${myName} process text to ${targetType}.`);
             break;
         case "proper case":
-
+            convertedText = text.toLowerCase()
+            convertedText = convertedText.replace(/(^\w|\s\w)/g, c => c.toUpperCase());
+            // /(^\w{1})|(\s+\w{1})/g // ok, from https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+            // /\b\w/g  wrong, changes also d of abc'd
+            // /(\s\w|^\w)/g // works, by Peter
             break;
-        case "sequence case":
-
+        case "sentence case":
+            convertedText = text.toLowerCase()
+            convertedText = convertedText.replace(/(^\w|\.\s\w)/g, c => c.toUpperCase());
+            // /(^\w|\.\s\w)/g // works on simple test sentence only!
             break;
         default:
             console.log(`Unknown target type ${targetType}. Can't process conversion.`);
